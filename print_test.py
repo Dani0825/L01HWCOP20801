6196ca61c307db92bdf90ef94ed503b414928677
Dani0825 <156130469+Dani0825@users.noreply.github.com>
2026-01-13 00:00:45 +0000

    HW1

---
+++ print_test.py
@@ -0,0 +1,5 @@
+my_name = "Daniela Huber"
+print(my_name)
+print("Use 4 spaces per indentation level")
+print("A Python program is divided into a number of logical lines.")
+print("I hope to improve my problem solving skills, become better at Python and learn about API.")
